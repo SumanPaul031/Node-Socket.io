f7f78aae364abe28a5d29d419fc984519c41e27c
sumanpaul2031@gmail.com <sumanpaul2031@gmail.com>
2020-07-11 00:51:49 +0530

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("A new user just connected");
+
+    socket.on('disconnect', () => {
+        console.log("A user just disconnected");
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server is up on Port ${port}`);
 })
